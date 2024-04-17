a1cd7b1442f78633e2006ae48fa092f15b776165
Pedro <pjg2@pdx.edu>
2024-04-17 09:59:49 -0700

    adding jest test folder and sample file

---
+++ lang2views-backend/src/__tests__/youtube.test.ts
@@ -0,0 +1,8 @@
+import { YouTube } from '../youtube'
+
+describe('YouTube API Tests', () => {
+    it('Default Test', () => {
+        let yt = new YouTube();
+        console.log("This is a default test")
+    })
+})
