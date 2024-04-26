c867039e6f353b0a3e420c23c2d6e37b8eea0665
Pedro <pjg2@pdx.edu>
2024-04-26 15:43:09 -0700

    typo

--- lang2views-backend/src/app.ts
+++ lang2views-backend/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 import { YouTube } from './youtube';
 import { Whisper } from './whisper';
-// import { Bing } from './bing';
+import { Bing } from './bing';
 import { Users } from './users'
 const app = express();
 const port = 3000;
