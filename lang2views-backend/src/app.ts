bcb8a949616d8d1378be6742d8a6837a00be04dc
Pedro <pjg2@pdx.edu>
2024-04-26 16:39:51 -0700

    reqest param checking

--- lang2views-backend/src/app.ts
+++ lang2views-backend/src/app.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import path from 'path';
 import { YouTube } from './youtube.js';
 import { Whisper } from './whisper.js';
 import { Bing } from './bing.js';
@@ -22,16 +21,20 @@ app.get('/', (req, res) => {
 // YouTube download API
 app.post('/youtube/download', async (req,res) => {
     let youtube = new YouTube();
-    let url = req.body.url;
-    let videoName = req.body.videoName;
-    await youtube.downloadVideo(url, videoName);
-    await youtube.downloadAudio(url, videoName, async (err) => {
-        if(!err){
-            res.send('Download Complete')
-        } else {
-            console.log(err);
-        }
-    });
+    const url = req.body.url;
+    const videoName = req.body.videoName;
+    if(url && videoName){
+        await youtube.downloadVideo(url, videoName);
+        await youtube.downloadAudio(url, videoName, async (err) => {
+            if(!err){
+                res.send('Download Complete')
+            } else {
+                console.log(err);
+            }
+        });
+    } else {
+        res.send('Invalid request body: Please send url and vidoeName')
+    }
 })
 
 // YouTube upload API
@@ -44,13 +47,17 @@ app.post('/whisper/transcribe', async (req,res) => {
     // Need to pass secret key for Whisper API usage
     const filePath = req.body.filePath;
     const whisperKey = req.body.whisperKey;
-    const decodedKey = Buffer.from(whisperKey, 'base64').toString('utf-8');
-    let whisper = new Whisper(decodedKey);
-    try{
-        let response = await whisper.transcribeAudio(filePath);
-        res.send(response);
-    } catch(e){
-        res.send(e);
+    if(filePath && whisperKey){
+        const decodedKey = Buffer.from(whisperKey, 'base64').toString('utf-8');
+        let whisper = new Whisper(decodedKey);
+        try{
+            let response = await whisper.transcribeAudio(filePath);
+            res.send(response);
+        } catch(e){
+            res.send(e);
+        }
+    } else {
+        res.send('Invalid request body: Please send filePath and whisperKey')
     }
 })
 
@@ -58,26 +65,38 @@ app.post('/whisper/transcribe', async (req,res) => {
 app.post('/bing/translate', async (req,res) => {
     let bing = new Bing();
     const text = req.body.translateText;
-    let response = await bing.translateText(text);
-    res.send(response);
+    if(text){
+        let response = await bing.translateText(text);
+        res.send(response);
+    } else {
+        res.send("Invalid request body: Please send translateText")
+    }
 })
 
 // Login API
 app.post('/user/login', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    let users = new Users(userFile);
-    let result = users.authenticate({username:username,password:password});
-    res.send(result);
+    if(username && password){
+        let users = new Users(userFile);
+        let result = users.authenticate({username:username,password:password});
+        res.send(result);
+    } else {
+        res.send('Invalid request body: Please send username and password');
+    }
 })
 
 app.post('/user/createUser', (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
-    let users = new Users(userFile);
-    let result = users.createNewUser({username:username,password:password});
-    users.writeUsersToFile();
-    res.send(result);
+    if(username && password){
+        let users = new Users(userFile);
+        let result = users.createNewUser({username:username,password:password});
+        users.writeUsersToFile();
+        res.send(result);
+    } else {
+        res.send('Invalid request body: Please send username and password');
+    }
 })
 
 app.post('/user/updateUser', (req, res) => {
@@ -85,22 +104,30 @@ app.post('/user/updateUser', (req, res) => {
     const oldPassword = req.body.oldPassword;
     const username = req.body.username;
     const password = req.body.password;
-    let users = new Users(userFile);
-    let result = users.updateUser({username:oldUsername, password:oldPassword},{username:username,password:password});
-    users.writeUsersToFile();
-    res.send(result);
+    if(oldUsername && oldPassword && username && password){
+        let users = new Users(userFile);
+        let result = users.updateUser({username:oldUsername, password:oldPassword},{username:username,password:password});
+        users.writeUsersToFile();
+        res.send(result);
+    } else {
+        res.send('Invalid request body: Please send username and password');
+    }
 })
 
 app.post('/user/removeUser', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    let users = new Users(userFile);
-    let result = users.removeUser({username:username, password:password});
-    if(result){
-        res.send('User removed.')
-        users.writeUsersToFile();
+    if(username && password){
+        let users = new Users(userFile);
+        let result = users.removeUser({username:username, password:password});
+        if(result){
+            res.send('User removed.')
+            users.writeUsersToFile();
+        } else {
+            res.send('User not found.')
+        }
     } else {
-        res.send('User not found.')
+        res.send('Invalid request body: Please send username and password');
     }
 })
 
