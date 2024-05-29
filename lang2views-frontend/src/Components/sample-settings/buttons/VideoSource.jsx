b306099429bc67bdf02a527ad8f21df4de346742
Worick046 <pdwood02@gmail.com>
2024-05-29 11:04:24 -0700

    Fixed file input value reading null.

--- lang2views-frontend/src/Components/sample-settings/buttons/VideoSource.jsx
+++ lang2views-frontend/src/Components/sample-settings/buttons/VideoSource.jsx
@@ -14,8 +14,8 @@ function UploadFile() {
   return (
     <div className="ms-5 side-by-side">
       <h2>Upload</h2>
-      <label htmlFor="UploadFile-input"></label>
-      <input className="form-control form-control-lg" id="UploadFile-input" type="file"></input>
+      <label htmlFor="uploadFile-input"></label>
+      <input className="form-control form-control-lg" id="uploadFile-input" type="file"></input>
     </div>
   );
 }
