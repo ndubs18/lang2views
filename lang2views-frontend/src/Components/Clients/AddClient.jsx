67f1eef4ce9a730a2193962d80f0e01440fa227e
AlexNovitchkovBurbank <ryzen71511@outlook.com>
2024-05-31 13:59:55 -0700

    Add and delete client functionality working

--- lang2views-frontend/src/Components/Clients/AddClient.jsx
+++ lang2views-frontend/src/Components/Clients/AddClient.jsx
@@ -1,10 +1,31 @@
-function saveButtonClick() {}
+import { useEffect } from "react";
+import { createRoot } from "react-dom/client";
+import ClientAndSampleCreationViews from "../../Pages/clientAndSampleCreationViews";
+
+function saveButtonClick() {
+  const channelNameInput = document.querySelector("#channelName");
+  const channelUrlInput = document.querySelector("#channelUrl");
+
+  fetch("http://localhost:3000/client/add", {
+    method: "POST",
+    body: {
+      url: channelUrlInput.textContent,
+    },
+  })
+    .then((response) => response.json())
+    .then((value) => console.log(value));
+
+  const root = document.querySelector("#root");
+  const addClientHook = createRoot(root);
+
+  addClientHook.render(<ClientAndSampleCreationViews />);
+}
 
 function SaveClient() {
   return (
     <div className="d-flex flex-row justify-content-center">
       <button className="btn btn-primary" onClick={saveButtonClick}>
-        Save
+        Add client
       </button>
     </div>
   );
@@ -12,15 +33,15 @@ function SaveClient() {
 
 function AddClient() {
   return (
-    <div className="d-flex flex-row justify-content-center">
+    <div className="d-flex flex-row justify-content-center mt-5">
       <div>
         <div className="mb-4">
           <h2>Channel name</h2>
-          <input id="channelName" />
+          <input className="form-control" id="channelName" />
         </div>
         <div className="mb-4">
           <h2>Channel url</h2>
-          <input id="channelUrl" />
+          <input className="form-control" id="channelUrl" />
         </div>
         <SaveClient />
       </div>
