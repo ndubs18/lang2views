e7723c4fe7850d24c3e975c92b4e21207985a981
juicysteak7 <83801343+juicysteak7@users.noreply.github.com>
2024-05-31 14:02:34 -0700

    Revert "Add client"

--- lang2views-frontend/src/App.jsx
+++ lang2views-frontend/src/App.jsx
@@ -1,18 +1,17 @@
-import ClientAndSampleCreationViews from './Pages/clientAndSampleCreationViews';
-import LongFormatVideoList from './Components/VideoList/LongFormatVideoList/longFormatVideoList';
-import ShortFormatVideoList from './Components/VideoList/ShortFormatVideoList/shortFormatVideoList';
-import LoginPage from './Pages/login';
-import Organize from './Components/VideoList/Organize/Organize';
-import PostProduction from './Components/VideoList/PostProduction/PostProduction';
-import Upload from './Components/VideoList/Upload/Upload';
-import { GlobalContextProvider } from './Context/globalContext.jsx';
-import AddClient from './Components/Clients/AddClient';
+import ClientAndSampleCreationViews from "./Pages/clientAndSampleCreationViews";
+import LongFormatVideoList from "./Components/VideoList/LongFormatVideoList/longFormatVideoList";
+import ShortFormatVideoList from "./Components/VideoList/ShortFormatVideoList/shortFormatVideoList";
+import LoginPage from "./Pages/login";
+import Organize from "./Components/VideoList/Organize/Organize";
+import PostProduction from "./Components/VideoList/PostProduction/PostProduction";
+import Upload from "./Components/VideoList/Upload/Upload";
+import { GlobalContextProvider } from "./Context/globalContext.jsx";
 
 function App() {
   return (
     <>
       <GlobalContextProvider>
-        <AddClient />
+        <LoginPage />
         {/*<ClientAndSampleCreationViews />*/}
         {/*<LongFormatVideoList />*/}
       </GlobalContextProvider>
