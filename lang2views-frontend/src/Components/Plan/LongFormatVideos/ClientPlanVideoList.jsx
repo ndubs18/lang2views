1c71f28d3519718027fa9bfefb352c46dba1740a
Nevada Duyck <njduyck@gmail.com>
2024-06-02 20:16:32 -0700

    Hook up client planning to backend; condense filtering options; condense long/short format differences into one component

---
+++ lang2views-frontend/src/Components/Plan/LongFormatVideos/ClientPlanVideoList.jsx
@@ -0,0 +1,195 @@
+import React, { useEffect, useState } from "react";
+import Video from "./Video";
+import Save from "./Buttons/LongFormatStepSave";
+import "../clientPlan.css";
+import {
+  sortDurationMostToLeast,
+  sortViewsMostToLeast,
+  sortViewsPerMinuteMostToLeast,
+} from "../../Utilities/sortAscending";
+import handlePreviousPageButtonClicked from "./Buttons/PreviousPageButtonClicked";
+import handleNextPageButtonClicked from "./Buttons/NextPageButtonClicked";
+
+const ACTIVE_BUTTON_CLASS = "filter-button-active";
+const INACTIVE_BUTTON_CLASS = "filter-button-inactive";
+
+function ClientPlanVideoList({ channelId, format }) {
+    const [getVideosResult, setGetVideosResult] = useState([]);
+    const [previousButtonClicked, setPreviousButtonClicked] = useState("");
+    const [nextButtonClicked, setNextButtonClicked] = useState("");
+
+    let currentFilter = "unsorted"
+    let pageToken = ""
+    useEffect(() => {
+        const currentNumberToProcess = document.querySelector(
+            "#current-number-to-process"
+        );
+
+        const currentVideosForProcessingContainer = document.querySelector(
+            "#videos-for-processing-json"
+        );
+        if (nextButtonClicked === "" && previousButtonClicked === "") {
+            currentVideosForProcessingContainer.textContent = JSON.stringify([]);
+            currentNumberToProcess.value = 0;
+        }
+
+        fetch("http://localhost:3000/client/getVideoPage", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                channelId: channelId,
+                pageToken: pageToken
+            }),
+        })
+            .then((response) => {
+                response.json().then((value) => setGetVideosResult(value)
+                )
+            }
+        )
+        .catch((err) => {
+        throw new Error(err);
+        });
+
+    if (previousButtonClicked === "true") {
+        setPreviousButtonClicked("false");
+    } else if (nextButtonClicked === "true") {
+        setNextButtonClicked("false");
+    }
+    }, [previousButtonClicked, nextButtonClicked]);
+
+    const currentNumberToProcess = document.querySelector(
+    "#current-number-to-process"
+    );
+
+    let videosUnsorted = [];
+    let videosByDuration = [];
+
+    if (getVideosResult.videos) {
+        let filteredVideos = getVideosResult.videos.filter(
+            (video) => video.format === format
+        );
+        filteredVideos.forEach((video) => {
+            let newVideo = <Video
+                key={video.id}
+                format={format}
+                video={video}
+            />
+            videosUnsorted.push(newVideo);
+        })
+
+        // Create copy that's sorted by duration
+        // Alternative to this is re-rendering entire thing when we want to switch filters
+        // (not great) or re-render child when list sort changes (couldn't get this to work
+        // + hard to go back to "unsorted" in that case)
+        videosByDuration = videosUnsorted.toSorted((a, b) => {
+            let aDuration = a.props.video.duration
+            let bDuration = b.props.video.duration
+            if (aDuration.minutes != bDuration.minutes) {
+                return bDuration.minutes - aDuration.minutes
+            } else {
+                return bDuration.seconds - aDuration.seconds
+            }
+        })
+    }
+
+    let viewsFilterActive = INACTIVE_BUTTON_CLASS;
+    let durationFilterActive = INACTIVE_BUTTON_CLASS;
+    let viewsPerMinuteFilterActive = INACTIVE_BUTTON_CLASS;
+
+    function handleDurationClick() {
+        let durationButton = document.getElementById("duration-button");
+        let durationContainer = document.getElementById("sorted-by-duration");
+        if (currentFilter == "duration") {
+            durationButton.classList.replace(ACTIVE_BUTTON_CLASS, INACTIVE_BUTTON_CLASS);
+            durationContainer.style.display = "none";
+            document.getElementById(`sorted-by-unsorted`).style.display = "grid";
+            currentFilter = "unsorted"
+        } else {
+            durationButton.classList.replace(INACTIVE_BUTTON_CLASS, ACTIVE_BUTTON_CLASS);
+            document.getElementById(`sorted-by-${currentFilter}`).style.display = "none";
+            durationContainer.style.display = "grid";
+            currentFilter = "duration"
+        }
+    }
+
+    function handleViewsClick() {
+
+    }
+    function handleViewsPerMinClick() {
+
+    }
+
+
+  return (
+    <>
+      <div className="d-flex flex-row ms-5 mb-5 filter-buttons-container">
+        <p className="align-middle fs-4 mt-2 me-4">Filters:</p>
+        <button
+          id={"duration-button"}
+          className={durationFilterActive + " btn"}
+          onClick={handleDurationClick}
+        >
+          Duration
+        </button>
+        <button
+          id={"views-button"}
+          className={viewsFilterActive + " btn"}
+          onClick={handleViewsClick}
+        >
+          Views
+        </button>
+        <button
+          id={"views-per-min-button"}
+          className={viewsPerMinuteFilterActive + " btn"}
+          onClick={handleViewsPerMinClick}
+        >
+          Views per minute
+        </button>
+      </div>
+        {/*
+        TODO : uncomment once videos can return views
+        <div id="sorted-by-views" style={{ display: "none" }} className="scrollable-video-list">{videosByViews}</div>
+        <div id="sorted-by-views-per-min" style={{ display: "none" }} className="scrollable-video-list">{videosByViewsPerMin}</div>
+        */}
+        <div id="sorted-by-duration" style={{ display: "none" }} className="scrollable-video-list">{videosByDuration}</div>
+        <div id="sorted-by-unsorted" className="scrollable-video-list">{videosUnsorted}</div>
+      <div
+        className="d-flex flex-row justify-content-center"
+        id="previous-next-videos-buttons-container"
+      >
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            setPreviousButtonClicked("true");
+            handlePreviousPageButtonClicked();
+            //setPageToken(pageOf50Node.prevPageToken);
+          }}
+        >
+          Previous
+        </button>
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            setNextButtonClicked("true");
+            handleNextPageButtonClicked();
+            //setPageToken(pageOf50Node.nextPageToken);
+          }}
+        >
+          Next
+        </button>
+      </div>
+      <div className="horizontal-line"></div>
+      <input
+        id="current-number-to-process"
+        hidden
+        type="number"
+        value={currentNumberToProcess ? currentNumberToProcess.value : 0}
+      />
+      <Save />
+    </>
+  );
+}
+
+export default ClientPlanVideoList;
---
+++ lang2views-frontend/src/Components/Plan/LongFormatVideos/Video.jsx
@@ -0,0 +1,76 @@
+import "../videos.css"
+function handleSelectButton(event) {
+    const currentVideoSelectButton = document.querySelector(`#${event.target.id}`);
+
+    if (currentVideoSelectButton.textContent === "") {
+        const currentNumber = document.querySelector("#current-number-to-process");
+
+        currentNumber.value = Number.parseInt(currentNumber.value) + 1;
+
+        const buttonsWithSameIdAsCurrent = document.querySelectorAll(`#${event.target.id}`);
+
+        buttonsWithSameIdAsCurrent[0].textContent = currentNumber.value;
+        buttonsWithSameIdAsCurrent[1].textContent = currentNumber.value;
+        buttonsWithSameIdAsCurrent[2].textContent = currentNumber.value;
+        buttonsWithSameIdAsCurrent[3].textContent = currentNumber.value;
+    }
+    else {
+        const allVideosButtons = document.querySelectorAll(".long-format-video-select-button");
+
+        const currentNumber = document.querySelector("#current-number-to-process");
+
+        currentNumber.value = Number.parseInt(currentNumber.value) - 1;
+
+        const placementOfCurrentVideoInProcessingOrder = currentVideoSelectButton.textContent;
+
+        for (let i = 0; i < allVideosButtons.length; i++) {
+            if (allVideosButtons[i].id === currentVideoSelectButton.id)
+                allVideosButtons[i].textContent = "";
+            else {
+                if (allVideosButtons[i].textContent !== "" && allVideosButtons[i].textContent > placementOfCurrentVideoInProcessingOrder)
+                    allVideosButtons[i].textContent = Number.parseInt(allVideosButtons[i].textContent) - 1;
+            }
+        }
+    }
+}
+
+function Video({ format, video }) {
+    let videoId = ""
+    let videoName = ""
+    let duration = ""
+    let finalized = ""
+    let thumbnailSrc = ""
+
+    if (video) {
+        videoId = video.id
+        videoName = video.name
+        duration = `${video.duration.minutes}:${video.duration.seconds}`
+        finalized = video.finalized
+        thumbnailSrc = video.thumbnail.url
+    }
+
+    return (
+        <div className={`d-flex flex-column video-container ${format}-seperation ${format=="short" ? "short-container" : ""}`}>
+            <div style={{ width: "100%", height: "100%", backgroundImage: `url(${thumbnailSrc})`}}>
+                <button className={`rounded rounded-circle ${format}-select-button`} onClick={handleSelectButton} id={videoId} disabled={finalized}></button>
+                <div id={videoId + "-thumbnail"}>{thumbnailSrc}</div>
+            </div>
+            <p>{videoName}</p>
+            <div className="d-flex flex-row">
+                {format == "long" ? (
+                    <>
+                        <p className="me-2 text-secondary">{duration} Min</p>
+                        <p className="me-2 text-secondary">{/*video.views*/0} Views</p>
+                        <p className="me-2 text-secondary">{/*video.viewsPerMinute*/0} Views/Min</p>
+                    </>
+                ) : (
+                        <p className="fs-4 me-2 text-secondary">{/*video.views*/0} Views</p>
+                )}
+
+            </div>
+        </div>
+    );
+}
+
+export default Video;
+
