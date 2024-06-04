227394beb4424f14a27296df2aa9986cabcb5597
Nevada Duyck <njduyck@gmail.com>
2024-06-03 18:01:29 -0700

    Fix multiple selections, plus issue where some YouTube video IDs start with a number (HTML doesn't like this?)

--- lang2views-frontend/src/Components/Plan/Video.jsx
+++ lang2views-frontend/src/Components/Plan/Video.jsx
@@ -47,9 +47,12 @@ function Video({ format, video }) {
     return (
         <div className={`d-flex flex-column video-container ${format}-seperation ${format=="short" ? "short-container" : ""}`}>
             <div style={{ width: "100%", height: "100%", backgroundImage: `url(${thumbnailSrc})`}}>
-                <button className={`rounded rounded-circle ${format}-select-button video-select-button`} onClick={handleSelectButton} id={videoId} disabled={finalized}>
-                </button>
-                <div id={`${videoId}-data`} style={{ display:"none" }}>{JSON.stringify(video)}</div>
+                <button
+                    id={`button-${videoId}`}
+                    className={`rounded rounded-circle ${format}-select-button video-select-button`}
+                    onClick={handleSelectButton}
+                    disabled={finalized}/>
+                <div id={`data-${videoId}`} style={{ display:"none" }}>{JSON.stringify(video)}</div>
                 <div id={videoId + "-thumbnail"}>{thumbnailSrc}</div>
             </div>
             <p>{videoName}</p>
