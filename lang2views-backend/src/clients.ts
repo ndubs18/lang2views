dd94fe9da3c4aa93d38dfc4352ae9cd83ae7e52a
Pedro <pjg2@pdx.edu>
2024-06-04 10:20:42 -0700

    now deletes everything

--- lang2views-backend/src/clients.ts
+++ lang2views-backend/src/clients.ts
@@ -40,10 +40,12 @@ export class Clients {
                 if(client.videos){
                     client.videos.markComplete(videoId);
                     client.videos.removeVideo(videoId);
+                    this.deleteFolderRecursive(`clients/${client.channelId}/${videoId}`);
                 } else {
                     client.videos = new Videos('clients/'+client.channelId+'/videos.json');
                     client.videos.markComplete(videoId);
                     client.videos.removeVideo(videoId);
+                    this.deleteFolderRecursive(`clients/${client.channelId}/${videoId}`);
                 }
                 client.videos.writeVideosToFile();
             }
