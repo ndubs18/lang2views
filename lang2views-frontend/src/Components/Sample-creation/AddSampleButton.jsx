70d97665b6e5fadc8cc37a59ea315b487a516331
Worick046 <pdwood02@gmail.com>
2024-06-12 13:48:37 -0700

    Fixed redirection, back and forward browser arrows now work as expected.

--- lang2views-frontend/src/Components/Sample-creation/AddSampleButton.jsx
+++ lang2views-frontend/src/Components/Sample-creation/AddSampleButton.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import AddSample from "./AddSample";
-
-function addSampleButtonClick() {
-    const root = document.querySelector("#root");
-    const addSampleHook = createRoot(root);
-
-    window.history.replaceState({}, "addSample", "/addSample");
-    addSampleHook.render(<AddSample />);
-}
 
 function AddSampleButton() {
-    return <button className="btn btn-primary" id="add-sample-button" onClick={addSampleButtonClick}>+ Add Sample</button>
+    return (
+        <a href="/addSample" className="btn btn-primary" id="add-sample-button">
+            + Add Sample
+        </a>
+    );
 }
 
 export default AddSampleButton;
